959f3780e7792b4a69722e5e96d609875bc19a03
Eslam <eslamhussin600@gmail.com>
2025-10-03 03:58:04 +0300

    parasite controle done 🐏

--- src/modules/parasiteControl/parasiteControl.route.js
+++ src/modules/parasiteControl/parasiteControl.route.js
@@ -1,28 +1,14 @@
 import { Router } from "express";
 import * as parasiteControlController from "./parasiteControl.controller.js";
-import { isAuth } from "../../middleware/isAuth.js";
 
 const router = Router();
 
-// إنشاء تقرير
-router.post('/create', isAuth, parasiteControlController.createReport);
-
-// الحصول على جميع التقارير
-router.get('/', isAuth, parasiteControlController.getAllReports);
-
-// الحصول على إحصائيات
-router.get('/stats', isAuth, parasiteControlController.getStats);
-
-// فلترة التقارير
-router.get('/filter', isAuth, parasiteControlController.filterReports);
-
-// الحصول على تقرير واحد
-router.get('/:reportId', isAuth, parasiteControlController.getReportById);
-
-// تحديث تقرير
-router.put('/:reportId', isAuth, parasiteControlController.updateReport);
-
-// حذف تقرير
-router.delete('/:reportId', isAuth, parasiteControlController.deleteReport);
+router.post('/create', parasiteControlController.createReport);
+router.get('/', parasiteControlController.getAllReports);
+router.get('/stats', parasiteControlController.getStats);
+router.get('/filter', parasiteControlController.filterReports);
+router.get('/:reportId', parasiteControlController.getReportById);
+router.put('/:reportId', parasiteControlController.updateReport);
+router.delete('/:reportId', parasiteControlController.deleteReport);
 
 export default router;
